1d1cc201b762713e40a8f84806b4f9fd5253a77c
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-11 15:10:35 +0100

    Store datapoint count in variable

--- weather.py
+++ weather.py
@@ -7,7 +7,8 @@ def getHistoricalWeather():
         for line in f:
             api_key = line.split(None, 1)[0]
 
-    url = f"http://history.openweathermap.org/data/2.5/history/city?lat=53.3498&lon=6.2603&type=hour&cnt=2&appid={api_key}"
+    datapointCount = 10
+    url = f"http://history.openweathermap.org/data/2.5/history/city?lat=53.3498&lon=6.2603&type=hour&cnt={datapointCount}&appid={api_key}"
 
     r = requests.get(url) 
     json_data = json.loads(r.text)
