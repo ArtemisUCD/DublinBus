029d8d9adc866d47489600432a8241a6b6fa7f8a
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-11 15:03:00 +0100

    Scrape historical weather data

---
+++ weather.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# function to return historical weather data for Dublin
+def getHistoricalWeather():
+    with open("weatherKey.txt","r") as f:
+        for line in f:
+            api_key = line.split(None, 1)[0]
+
+    url = f"http://history.openweathermap.org/data/2.5/history/city?lat=53.3498&lon=6.2603&type=hour&cnt=2&appid={api_key}"
+
+    r = requests.get(url) 
+    json_data = json.loads(r.text)
+    return json_data
+
+
