9663a3ae9fc291de6c7e5b264192d821e1053f4e
20211258 <qiyan.xuan@ucdconnect.ie>
2022-06-12 22:19:07 +0100

    Escape

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './components/Header/Header'
-import Map from './components/Header/Map'
+import Map from './components/Map/Map'
+import { Wrapper } from "@googlemaps/react-wrapper";
+
+const render = Status => {
+  return <h1>{Status}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Map />
+      <Wrapper apiKey={"Google-API"} render={render}>
+        <Map />
+      </Wrapper>
     </div>
   );
 }
