ecfdf3a691d92013db3f257c33fc61b4a25fc202
21205925 <93437756+21205925@users.noreply.github.com>
2022-06-16 12:36:09 +0100

    new django app

---
+++ backend/buses/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+#from .models import Calendar, CalendarDates, Routes, Shapes, StopTimes, StopsTransfers, Trips
+
+#admin.site.register(Calendar, CalendarDates, Routes, Shapes, StopTimes, StopsTransfers, Trips)
---
+++ backend/buses/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Stops
+
+class StopsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stops
+        fields = ('stop_id', 'stop_name', 'stop_lat', 'stop_lon')
+
