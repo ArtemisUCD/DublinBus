a43c131c875de5be312f6b9baf0608b3b9a15036
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-16 14:27:35 +0100

    Scrape daily weather forecast

---
+++ store_daily_weather.py
@@ -0,0 +1,56 @@
+from weather import getDailyWeather
+from sqlalchemy import Column, Date, Integer, String,create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import urllib.parse
+import os
+from dotenv import load_dotenv
+
+# load environment variable file
+load_dotenv()
+
+# load weather data from openweather API
+weather_data = getDailyWeather()
+
+# Database details from env file
+USER = os.environ['MYSQL_USERNAME']
+PASSWORD = urllib.parse.quote_plus(os.environ['DUBLIN_BUS_PASSWORD'])
+PORT = os.environ['MYSQL_PORT']
+URI= os.environ['DUBLIN_BUS_ENDPOINT']
+DATABASE =  os.environ['MYSQL_DATABASE']
+
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{URI}:{PORT}/{DATABASE}")
+conn = engine.connect()
+
+Base = declarative_base()
+class Date(Base):
+    __tablename__ = 'daily_weather'
+    date = Column(String(32), primary_key=True, unique=True)
+    temperature = Column(String(32), default="0")
+    weather_icon=Column(String(32), default="None")
+    def __str__(self):
+        return self.id
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+# set up session to store data
+Session = sessionmaker(engine)
+db_session = Session()
+date_list = weather_data["list"]
+for day in date_list:
+    date= day["dt"]
+    temperature = day["temp"]["day"]
+    weather_icon = day["weather"][0]["icon"]
+
+    row = Date(
+    date = date,
+    temperature = temperature,
+    weather_icon=weather_icon,
+    )
+
+    # add row to session to be committed at end of dataset
+    db_session.add(row)
+db_session.commit() # commit dataset when all entries have been parsed
+conn.close()
+
--- weather.py
+++ weather.py
@@ -1,15 +1,23 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+api_key = os.environ['WEATHER_API_KEY']
 # function to return historical weather data for Dublin
 def getHistoricalWeather():
-    with open("weatherKey.txt","r") as f:
-        for line in f:
-            api_key = line.split(None, 1)[0]
-
     datapointCount = 10
-    url = f"http://history.openweathermap.org/data/2.5/history/city?lat=53.3498&lon=6.2603&type=hour&cnt={datapointCount}&appid={api_key}"
-
+    url = f"http://history.openweathermap.org/data/2.5/history/city?lat=53.3037&lon=-6.2324&type=hour&cnt={datapointCount}&appid={api_key}"
     r = requests.get(url) 
     json_data = json.loads(r.text)
     return json_data
+
+def getDailyWeather():
+    datapointCount = 7
+    url = f"http://api.openweathermap.org/data/2.5/forecast/daily?lat=53.303&lon=-6.232&cnt={datapointCount}&appid={api_key}"
+    r = requests.get(url) 
+    json_data = json.loads(r.text)
+    return json_data
+
