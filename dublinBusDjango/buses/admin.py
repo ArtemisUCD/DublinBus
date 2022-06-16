5dec486cf41dd65247625014b9261bb78ee0ddaf
21205925 <93437756+21205925@users.noreply.github.com>
2022-06-16 13:36:43 +0200

    redundant

--- dublinBusDjango/buses/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-#from .models import Calendar, CalendarDates, Routes, Shapes, StopTimes, StopsTransfers, Trips
-
-#admin.site.register(Calendar, CalendarDates, Routes, Shapes, StopTimes, StopsTransfers, Trips)
