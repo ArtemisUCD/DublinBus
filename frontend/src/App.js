04417846264afffe3b1e13a1222745192c2dc888
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-06-20 20:54:09 +0100

    Remove API key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Menu from './components/Menu/Menu'
-import Map from './components/Map/Map';
+import Map from './components/Map/Map'
+import { Wrapper } from "@googlemaps/react-wrapper";
 
+const render = Status => {
+  return <h1>{Status}</h1>
+}
 
 function App() {
-
-
 
   return (
     <div className="App">
       <Header />
       <div className='main-content'>
         <Menu />
+      <Wrapper apiKey={"API_KEY"} render={render}>
         <Map />
+      </Wrapper>
       </div>
     </div>
   );
