d3cac2e8d15c3c8c16fe4c1290fdcfd1d11b1a56
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-07-04 16:35:43 +0100

    Add tab menu to menu component

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -1,38 +1,38 @@
-import MenuItem from "./MenuItem";
 import './Menu.css'
-import { useState, useEffect } from "react";
+import RoutePlanner from "./RoutePlanner";
+import { useState } from "react";
+import { Box, Tab} from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
+import Tabs from '@mui/material/Tabs';
+import TabPanel from '@mui/lab/TabPanel';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
-const Menu = () => {
+const Menu = (props) => {
 
-    const menuItems = ["Real Time Information","Bus Route","Favourites", "Traffic Network PDF"];
-    const [favouriteRoutes, setFavouriteRoutes] = useState(JSON.parse(localStorage.getItem('favouriteRoutes')) ?? []);
+  const [value, setValue] = useState('1');
 
-    // update favourites in local storage when the state updates
-    useEffect(() => {
-    localStorage.setItem('favouriteRoutes', JSON.stringify(favouriteRoutes));
-    }, [favouriteRoutes]);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    // function to add bus route as a favourite
-    const addFavourite = (newRoute) =>{
-        if(!favouriteRoutes.includes(newRoute)){
-        setFavouriteRoutes((prevFavouriteRoutes) =>{
-          return [...prevFavouriteRoutes,newRoute]
-        })
-        }
-      }
-      
-    // function to remove bus route from list of favourites
-      const removeFavourite = (busRoute) =>{
-        setFavouriteRoutes((prevFavouriteRoutes) =>{
-          return prevFavouriteRoutes.filter(route => route!== busRoute)
-        })}
-      
-      let menuContent = menuItems.map(item => <MenuItem key ={item} onLike={addFavourite} onUnlike={removeFavourite} favourites= {favouriteRoutes} title = {item}/>)
-      console.log("Initial local",favouriteRoutes)
-    return (
-        <div className="main-menu">
-            {menuContent}
-        </div>
+return(
+  <Box className="main-menu" sx={{ zIndex:"1", position:"absolute", typography: 'body1', backgroundColor:"white",borderRadius:"10px"}}>
+  <TabContext value={value}>
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Tabs value={value} variant="scrollable" onChange={handleChange} aria-label="lab API tabs example">
+        <Tab icon={<DirectionsIcon />} label="Directions" value="1" />
+        <Tab icon={<DirectionsBusIcon />} label="Bus Routes" value="2" />
+        <Tab icon={<AccessTimeIcon />} label="Real Time Info" value="3" />
+      </Tabs>
+    </Box>
+    <TabPanel value="1" ><RoutePlanner origin={props.origin} getAddress ={props.getAddress}destination={props.destination} calcRoute={props.calcRoute} clearDetails={props.clearDetails} swap={props.swap} toggleDrawer={props.toggleDrawer}/>
+  </TabPanel>
+    <TabPanel value="2">Plot Bus Routes</TabPanel>
+    <TabPanel value="3">Real Time Information</TabPanel>
+  </TabContext>
+</Box>
     )
 }
 
--- frontend/src/components/Menu/RoutePlanner.js
+++ frontend/src/components/Menu/RoutePlanner.js
@@ -4,7 +4,7 @@ import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import {Autocomplete} from '@react-google-maps/api'
 import './RoutePlanner.css'
-import MenuIcon from '@mui/icons-material/Menu';
+
 
 const RoutePlanner = (props) => {
 
@@ -23,49 +23,44 @@ const RoutePlanner = (props) => {
     const swapInputFields = () => {
         props.swap();
     }
-    const toggleDrawer = () =>{
-        props.toggleDrawer()
-      }
+
 
     const mapBounds = {componentRestrictions:{country:["ie"]}}
 
-
     return(
-        <Box sx={{ display:'flex', flexDirection:"column",
-        position:"absolute",zIndex:"1",backgroundColor:"white",marginTop:"2rem",marginLeft:"1rem",
-        borderRadius:"10px;",alignItems:"flex-start"}}>
-                            <Box>
-                <IconButton onClick={toggleDrawer}>
-                <MenuIcon />
-            </IconButton>
-                </Box>
-                <Box sx={{height:"50%",width:"100%",display:"flex",marginTop:"1rem",
-                flexDirection:"column",justifyContent:"space-evenly",alignItems:"center"}}>
-                <Box sx={{display:"flex",alignItems:"center",justifyContent:"center",paddingBottom:"1rem"}}>
-                <Autocomplete options={mapBounds}>
-                <TextField size="small"style={{minWidth:100,maxWidth:400,width:"90%"}}id="outlined-basic" label="Origin" variant="outlined"  inputRef={props.origin} />
-                </Autocomplete>
-                <IconButton size="small" onClick={getAddress} sx={{border: "2px solid gray", borderRadius: 1}}>
-                <MyLocationIcon/>
-                </IconButton>
-                </Box>
-                <Box sx={{display:"flex",alignItems:"center",justifyContent:"flex-end",padding:"1rem"}}>
-                <Autocomplete options={mapBounds}>
-                <TextField size="small" sx={{ minWidth:100,maxWidth:400, width:"90%"}}id="outlined-basic" label="Destination" variant="outlined" inputRef={props.destination}/>
-                </Autocomplete>
-                <IconButton size ="small" onClick={swapInputFields} sx={{border: "2px solid gray", borderRadius: 1}}>
-                <SwapHorizIcon/>
-                </IconButton>
-                </Box>
-                <Box>
-                <Button onClick={calcRoute}variant="outlined" size="small" >Calculate Route</Button>
-                <IconButton onClick={clearDetails}>
-                <HighlightOffOutlinedIcon/>
-                </IconButton>
-                </Box>
-                </Box>
+
+        <Box sx={{ display:'flex', flexDirection:"column",zIndex:"1",backgroundColor:"white",marginLeft:"1rem",
+borderRadius:"10px;"}}>
+        <Box sx={{height:"50%",display:"flex",marginTop:"1rem",
+        flexDirection:"column",}}>
+        <Box sx={{display:"flex",paddingBottom:"1rem",justifyContent:"flex-start"}}>
+        <Autocomplete options={mapBounds}>
+        <TextField size="small"style={{minWidth:100,maxWidth:400,width:"90%"}}id="outlined-basic" label="Origin" variant="outlined"  inputRef={props.origin} />
+        </Autocomplete>
+        <IconButton size="small" onClick={getAddress} sx={{border: "2px solid gray", borderRadius: 1}}>
+        <MyLocationIcon/>
+        </IconButton>
         </Box>
+        <Box sx={{display:"flex",justifyContent:"flex-start"}}>
+        <Autocomplete options={mapBounds}>
+        <TextField size="small" sx={{ minWidth:100,maxWidth:400, width:"90%"}}id="outlined-basic" label="Destination" variant="outlined" inputRef={props.destination}/>
+        </Autocomplete>
+        <IconButton size ="small" onClick={swapInputFields} sx={{border: "2px solid gray", borderRadius: 1}}>
+        <SwapHorizIcon/>
+        </IconButton>
+        </Box>
+        <Box>
+        <Button onClick={calcRoute}variant="outlined" size="small" >Calculate Route</Button>
+        <IconButton onClick={clearDetails}>
+        <HighlightOffOutlinedIcon/>
+        </IconButton>
+        </Box>
+        </Box>
+</Box>
+
+   
     )
 }
+
 
 export default RoutePlanner;
