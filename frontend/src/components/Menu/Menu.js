2e7ac9aac7f352015c40dd20a1bfbc82c2f881a8
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-07-12 19:59:00 +0100

    Refactor route planner components

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import './Menu.css'
 import RoutePlanner from "./RoutePlanner";
+import RouteItem from './RouteItem'
 import { useState } from "react";
 import { Box, Tab} from "@mui/material";
 import TabContext from '@mui/lab/TabContext';
@@ -7,17 +8,14 @@ import Tabs from '@mui/material/Tabs';
 import TabPanel from '@mui/lab/TabPanel';
 import DirectionsIcon from '@mui/icons-material/Directions';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import Accordion from '@mui/material/Accordion';
 import BusRouteList from './BusRouteList';
 import RealTime from './RealTime';
-import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
 
 
 const Menu = (props) => {
-
+  let journeyDetails;
+  let routeList;
 
   const [value, setValue] = useState('1');
   const [startTime, setStartTime] = useState();
@@ -30,30 +28,24 @@ const Menu = (props) => {
     setStartTime(value)
   }
 
-
-  let journeyDetails;
-  let journeySummary;
-
   if(props.directions)
-{
-
-  journeyDetails = props.directions.routes.map(route => route.legs[0].steps.map((step)=>
+{journeyDetails = props.directions.routes.map(route => route.legs[0].steps.map((step)=>
   {if(step.travel_mode==="TRANSIT")
-  {return {distance:step.distance.text,
+  {return {
+      distance:step.distance.text,
       duration:step.duration.text,
       mode:step.travel_mode,
-  busNumber:step.transit.line.short_name,
-  headsign:step.transit.headsign,
-  stopCount:step.transit.num_stops,
-  departure:step.transit.departure_stop.name,
-  arrival:step.transit.arrival_stop.name}}
-      else{
-          return {distance:step.distance.text,
-              duration:step.duration.text,
-              mode:step.travel_mode}
+      busNumber:step.transit.line.short_name,
+      headsign:step.transit.headsign,
+      stopCount:step.transit.num_stops,
+      departure:step.transit.departure_stop.name,
+      arrival:step.transit.arrival_stop.name}}
+  else{
+  return {
+      distance:step.distance.text,
+      duration:step.duration.text,
+      mode:step.travel_mode}
       }}))
-
-
 
     // get duration each step of the journey takes
     let timings = journeyDetails.map(route => route.map(step => parseInt(step.duration.split(" ")[0])))
@@ -61,41 +53,9 @@ const Menu = (props) => {
     const cumulativeSum = (sum => value => sum += value);
     let stepTimes = timings.map(route => route.map(cumulativeSum(0)))
     // get datetime objects for timings and add starttime as first element
-    let answer = stepTimes.map(route => [new Date(startTime.getTime())].concat(route.map(duration => new Date(startTime.getTime() + duration * 60000))));
+    let routeTimings = stepTimes.map(route => [new Date(startTime.getTime())].concat(route.map(duration => new Date(startTime.getTime() + duration * 60000))));
 
-
-  journeySummary =journeyDetails.map((route,routeIndex)=> {return <Accordion>
-    <AccordionSummary 
-    aria-controls="panel1a-content"
-    sx={{width:"100%",display:"flex",paddingLeft:"2rem"}}
-    >{route.map((stepObj) =>{return <Box sx={{display:"flex"}}> {stepObj.mode==="WALKING"?<DirectionsWalkIcon/>:<DirectionsBusIcon/>} {stepObj.busNumber?<Box sx={{backgroundColor:"#FBCB0A",marginRight:"0.5rem",borderRadius:"5px",padding:"0.2rem"}}>{stepObj.busNumber}</Box>:null}</Box>}).reduce((prev, curr) => [prev, ' > ', curr])}
-    </AccordionSummary>
-    <AccordionDetails sx={{backgroundColor:"white"}}>
-    {journeyDetails[routeIndex].map((step,index) =>{if(step.mode==="WALKING")
-    { return <Box sx={{display:"flex",alignItems:"center", borderBottom:"1px solid black",padding:"1rem 0"}}>
-        <Box>{answer[routeIndex][index].getHours()}:{answer[routeIndex][index].getMinutes()<10?'0'+ answer[routeIndex][index].getMinutes():answer[routeIndex][index].getMinutes()}<DirectionsWalkIcon sx={{marginLeft:"1rem"}}/>
-        </Box>
-        <Box sx={{flexDirection:"column",marginLeft:"1rem"}}>
-        <p style={{margin:"0"}}>Walk</p>
-        <p style={{margin:"0"}}>About {step.duration}, {step.distance}</p>
-        </Box>
-      </Box>}
-    else{
-      return <Box sx={{display:"flex",alignItems:"center",borderBottom:"1px solid black",paddingBottom:"1rem"}}>
-        <Box>
-        MODEL<DirectionsBusIcon/>
-        </Box>
-        <Box sx={{flexDirection:"column",marginLeft:"1rem"}}>
-        <p>{step.departure}</p>
-        <Box sx={{display:"flex",alignItems:"center"}}><Box sx={{backgroundColor:"#FBCB0A",marginRight:"0.5rem",borderRadius:"5px",padding:"0.2rem"}}>{step.busNumber}</Box> {step.headsign}</Box>
-        <p>**MODEL VALUE** {step.stopCount} stops</p>
-        <p> {step.arrival}</p>
-        </Box>
-        </Box>
-    }}
-    )}
-    </AccordionDetails>
-    </Accordion>})
+  routeList =journeyDetails.map((routeObj,routeIndex)=> <RouteItem key={Math.random()}routeObj={routeObj} routeIndex={routeIndex} routeTimings={routeTimings}/>)
 }
 
 return(
@@ -117,7 +77,7 @@ return(
   </TabContext>
 </Box>
 <Box sx={{zIndex:"1", backgroundColor:"white",borderRadius:"10px",overflowY:"auto"}}>
-{journeyDetails && value==="1" ? journeySummary:null}
+{journeyDetails && value==="1" ? routeList:null}
 </Box>
 </Box>
     )
---
+++ frontend/src/components/Menu/RouteSummary.js
@@ -0,0 +1,23 @@
+import AccordionSummary from '@mui/material/AccordionSummary';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import { Box } from "@mui/material";
+
+const RouteSummary = (props) => {
+
+    return(
+        <AccordionSummary key={Math.random()}
+        aria-controls="panel1a-content"
+        sx={{width:"100%",display:"flex",paddingLeft:"2rem"}}
+        >{props.routeObj.map((stepObj) =>{
+            return <Box key={Math.random()} sx={{display:"flex"}}>
+            {stepObj.mode==="WALKING"?<DirectionsWalkIcon/>:<DirectionsBusIcon/>} {stepObj.busNumber?
+            <Box sx={{backgroundColor:"#FBCB0A",marginRight:"0.5rem",borderRadius:"5px",padding:"0.2rem"}}>
+                {stepObj.busNumber}
+                </Box>:null}
+            </Box>}).reduce((prev, curr) => [prev, ' > ', curr])}
+        </AccordionSummary>
+    )
+}
+
+export default RouteSummary;
