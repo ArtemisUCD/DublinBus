30cf07635c33eff631de3f5888f0e01dc976e059
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-07-20 17:20:49 +0100

    Add loading animation while awaiting model value

--- frontend/src/components/Menu/RoutePlanner/RouteItem.js
+++ frontend/src/components/Menu/RoutePlanner/RouteItem.js
@@ -1,7 +1,9 @@
 import Accordion from '@mui/material/Accordion';
 import RouteSummary from './RouteSummary';
 import RouteDetails from './RouteDetails'
+import { Box} from "@mui/material";
 import { useState } from 'react';
+import ReactLoading from "react-loading";
 
 const RouteItem = (props)=> {
 
@@ -15,7 +17,7 @@ const RouteItem = (props)=> {
     let routeDetails;
     
     if(props.routeTimings===undefined|| props.stepTimings===undefined){
-        routeDetails="Model data loading"
+        routeDetails=<Box sx={{display:"flex",justifyContent:"center"}}><p>Modelling Journey</p><ReactLoading type="bubbles" color="#000000" height={100} width={50}/ ></Box>
     }
     else{routeDetails = <RouteDetails key ={Math.random} routeObj={props.routeObj} routeIndex={props.routeIndex} routeTimings={props.routeTimings} stepTimings={props.stepTimings}/>}
 
