a993e38fbee186474268a79a906c89aac175cf4a
21205925 <93437756+21205925@users.noreply.github.com>
2022-07-27 16:09:08 +0200

    test urls

---
+++ backend/buses/tests/test_views.py
@@ -0,0 +1,13 @@
+# from django.test import TestCase, Client
+# from buses.urls import urlpatterns 
+# from buses.models import Stops, DailyWeather, BusesUpdates, Trips, StopTimes, Shapes, Routes,HourlyWeather 
+
+# class testViews(TestCase):
+
+
+#     def test_getBusRouteList(self):
+#         self.client = Client()
+#         self.list_url = urlpatterns('getBusRouteList')
+#         response = self.client.get(self.list_url)
+#         self.assertEquals(response.status_code, 200)
+        
