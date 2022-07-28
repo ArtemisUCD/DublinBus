e6d838ed51a21877bf995b780706a44861be7087
21205925 <93437756+21205925@users.noreply.github.com>
2022-07-28 12:25:03 +0200

    nothing special

--- backend/buses/tests/test_views.py
+++ backend/buses/tests/test_views.py
@@ -1,13 +1,19 @@
-# from django.test import TestCase, Client
-# from buses.urls import urlpatterns 
-# from buses.models import Stops, DailyWeather, BusesUpdates, Trips, StopTimes, Shapes, Routes,HourlyWeather 
+from django.test import TestCase, Client
+from django.urls import reverse
+from buses.urls import urlpatterns 
+from buses.models import Stops, DailyWeather, BusesUpdates, Trips, StopTimes, Shapes, Routes,HourlyWeather 
+from buses.views import getShape, getStopsForRoute, getUpdatesForStop, getBusRouteList,getBusStopList, getEstimateTime, getEstimateTime
 
-# class testViews(TestCase):
+import json
 
+class testViews(TestCase):
 
-#     def test_getBusRouteList(self):
-#         self.client = Client()
-#         self.list_url = urlpatterns('getBusRouteList')
-#         response = self.client.get(self.list_url)
-#         self.assertEquals(response.status_code, 200)
+    def setUp(self):
+        # self.client = Client()
+        self.stop1 = Stops.objects.create(stop_id = "rr",stop_name = "ff",stop_lat = "22",stop_lon = "22")
+
+    def test_getBusStopList_GET(self):
+        response = self.client.get('/buses/getBusStopList/')
+        # print(getBusStopList('request'))
+        self.assertEqual(response.status_code, 200)
         
