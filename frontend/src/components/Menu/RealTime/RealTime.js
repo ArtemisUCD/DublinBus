7d6034580dd415eeb831739770fce2b74e7e8365
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-08-05 22:50:08 +0100

    Add loading screens to real time component

--- frontend/src/components/Menu/RealTime/RealTime.js
+++ frontend/src/components/Menu/RealTime/RealTime.js
@@ -18,13 +18,13 @@ const RealTime = ({favouritesS,onLike,onUnlike,getData,getCenter,getZoom}) => {
 
     const [stopList, setStopList] =useState([]);
     const [showinfo, setShowinfo] = useState(false);
-    const [realTimeData, setRealTimeData] = useState([]);
-    const [value, setValue] = useState("");
-    const [stopId, setStopId] = useState();
+    const [realTimeData, setRealTimeData] = useState();
     const [stopSelected, setStopSelected] = useState([]);
+    const [fetchingData,setFetchingData] = useState(false)
     const [showfavicon, setshowfavicon] = useState(false);
-    const [center, setCenter] = useState({lat: 53.306221, lng: -6.21914755});
-    const [zoom, setZoom] = useState(11)
+    // const [zoom, setZoom] = useState(11)
+
+    const [realTimeDetails, setRealTimeDetails] = useState();
     
     useEffect(() => {
         fetch("/api/stop_")
@@ -33,20 +33,23 @@ const RealTime = ({favouritesS,onLike,onUnlike,getData,getCenter,getZoom}) => {
       },[]);
 
       useEffect(() => {
-        fetch("/buses/getUpdatesForStop/"+stopId+"/")
+        if(stopSelected.length===1){
+        // show favourite button beside the newly selected stop
+        setshowfavicon(true);
+        getData(stopSelected);
+        // after stop is selected fetch the relevant real time information
+        fetch("/buses/getUpdatesForStop/"+stopSelected[0].stop_id+"/")
         .then(response => response.json())
         .then(data => setRealTimeData(data))
-      },[stopId]);
+        }
+      },[stopSelected,getData]);
 
       useEffect(()=>{
-        if(realTimeData!==undefined && stopSelected!==undefined&& value !== ""){
+        if(realTimeData!==undefined){
+        setFetchingData(false)
         setShowinfo(true);
-        getData(stopSelected);
-        getCenter(center);
-        getZoom(zoom);
-        setshowfavicon(true);
       }
-    },[realTimeData,stopSelected, favouritesS,getData,getCenter,getZoom,center,zoom,value])
+    },[realTimeData])
 
 
     const toggleFavourite = (busStop) =>{
@@ -57,6 +60,72 @@ const RealTime = ({favouritesS,onLike,onUnlike,getData,getCenter,getZoom}) => {
             onUnlike(busStop)
         } 
     }
+
+    const fetchRealTime = (value)=>{
+        if(value!==null){
+        setFetchingData(true);
+        setStopSelected([{"stop_name": value.stop_name,
+                    "stop_id": value.stop_id,
+                    "stop_lat": value.stop_lat,
+                    "stop_lon": value.stop_lon}])
+        getCenter({"lat": value.stop_lat, "lng": value.stop_lon});
+        getZoom(13);
+        }
+        else{
+            setShowinfo(false);
+            setshowfavicon(false);
+        }
+    }
+
+
+    useEffect(()=>{
+        if(fetchingData){
+            setRealTimeDetails(<Box sx={{display:"flex",justifyContent:"center"}}><p>Fetching Real Time</p><ReactLoading type="bubbles" color="#000000" height={100} width={50}/></Box>)
+
+        }
+        else if(showinfo && !fetchingData){
+            setRealTimeDetails(        <Box sx={{display:"flex",paddingBottom:"1rem",justifyContent:"flex-start"}}>
+            <Box sx={{display:"flex", justifyContent:"flex-start", width:"100%"}}>
+        <TableContainer component={Paper}
+            sx={{maxHeight:400,}}>
+            <Table sx={{width: '100%',
+            maxWidth: 380,
+            position: 'relative',
+            overflow: 'auto',
+            maxHeight: 300, }} aria-label="simple table" stickyHeader>
+                <TableHead>
+                <TableRow>
+                    <TableCell>Bus Route</TableCell>
+                    <TableCell align="left">Details</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {realTimeData.map((row, index) => (
+                    <TableRow
+                    key={index}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell sx={{padding:"1rem 0.25rem 1rem 0.5rem"}} component="th" scope="row">
+                        {row.concat_name}
+                    </TableCell>
+                    <TableCell sx={{borderBottom:"1px solid rgba(224, 224, 224, 1)",padding:"1rem 0.25rem 1rem 0.5rem"}}align="left">Scheduled:<strong>{row.planned_arrival_time}</strong><br/>Actual:<strong>{row.estimated_arrival_delay_min}</strong></TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+    </Box> 
+        </Box>)
+        }
+        else{
+            setRealTimeDetails(null)
+        }
+
+    },[realTimeData,showinfo,fetchingData])
+
+
+
+
 
     return(
         <Box sx={{ display:'flex', flexDirection:"column",zIndex:"1",backgroundColor:"white",marginLeft:"1rem", maxheight:"300px",
@@ -77,14 +146,7 @@ const RealTime = ({favouritesS,onLike,onUnlike,getData,getCenter,getZoom}) => {
                             option.stop_name === value.stop_name
                         }
                         onChange={(e,value) => {
-                            setValue(value.stop_name); 
-                            setStopId(value.stop_id);
-                            setCenter({"lat": value.stop_lat, "lng": value.stop_lon})
-                            setZoom(13)
-                            setStopSelected([{"stop_name": value.stop_name,
-                                        "stop_id": value.stop_id,
-                                        "stop_lat": value.stop_lat,
-                                        "stop_lon": value.stop_lon}])
+                                fetchRealTime(value);
                             }}
                             
                         noOptionsText={<Box sx={{display:"flex",alignItems:"center"}}>Bus Stops loading<ReactLoading type="bubbles" color="#000000" height={50} width={50}/></Box>}
@@ -102,43 +164,8 @@ const RealTime = ({favouritesS,onLike,onUnlike,getData,getCenter,getZoom}) => {
                         </div>
                     )}
                 </Box>
-                {showinfo && (
-                    <Box sx={{display:"flex",paddingBottom:"1rem",justifyContent:"flex-start"}}>
-                        <Box sx={{display:"flex", justifyContent:"flex-start", width:"100%"}}>
-                   
-                    <TableContainer component={Paper}
-                        sx={{maxHeight:400,}}>
-                        <Table sx={{width: '100%',
-                        maxWidth: 380,
-                        position: 'relative',
-                        overflow: 'auto',
-                        maxHeight: 300, }} aria-label="simple table" stickyHeader>
-                            <TableHead>
-                            <TableRow>
-                                <TableCell>Bus Route</TableCell>
-                                <TableCell align="left">Details</TableCell>
-                            </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {realTimeData.map((row, index) => (
-                                <TableRow
-                                key={index}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                <TableCell sx={{padding:"1rem 0.25rem 1rem 0.5rem"}} component="th" scope="row">
-                                    {row.concat_name}
-                                </TableCell>
-                                <TableCell sx={{borderBottom:"1px solid rgba(224, 224, 224, 1)",padding:"1rem 0.25rem 1rem 0.5rem"}}align="left">Scheduled:<strong>{row.planned_arrival_time}</strong><br/>Actual:<strong>{row.estimated_arrival_delay_min}</strong></TableCell>
-                                </TableRow>
-                            ))}
-                            </TableBody>
-                        </Table>
-                        </TableContainer>
-                </Box> 
-                    </Box>
-                )}
+                {realTimeDetails}
                 </Box>
-    
                 </Box>
     )
 }
